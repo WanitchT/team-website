2d74957e00705e8a985e450fdff378b5e38e4962
Wanitch <wanitch@Wanitchs-MacBook-Pro.local>
2020-07-01 08:16:49 +0700

    cleancode

---
+++ js-workshop/cleancode.js
@@ -0,0 +1,21 @@
+// TODO
+
+const fruits = ['apple','banana','orange']
+
+    const fruitsCopy = [...fruits];
+
+    fruitsCopy.push("pineapple")
+
+    console.log(fruits)
+    console.log(fruitsCopy)
+
+
+    const students = [ { id:1 ,name : 'bean', age : 14},{id:2 , name : 'ken', age : 15}]
+
+    const studentsCopy = [...students]
+
+    obj1 = {id:3 ,name : 'jack'}
+    studentsCopy.push(obj1)
+
+    console.log(students)
+    console.log(studentsCopy)
